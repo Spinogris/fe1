36015999b26427c6b1f5cb8a9e5d1028baa52192
Spinogris <135016366+Spinogris@users.noreply.github.com>
2023-06-25 22:54:34 +0200

    Add files via upload

---
+++ 6/scripts/script.js
@@ -0,0 +1,39 @@
+// -----------------------------------------------------------
+
+// const number = +prompt("Enter number");
+// const procent = number * 0.1;
+// alert(procent);
+
+// -----------------------------------------------------------
+
+// const num1 = +prompt('Enter first number')
+// const num2 = +prompt('Enter second number')
+// if (num1 > num2){
+//     minValue = num2
+// } else if (num1 < num2){
+//     minValue = num1
+// }else {
+//     alert('Entered Values are equal')
+// }
+// alert(minValue);
+
+// -----------------------------------------------------------
+
+// const num = +prompt('Enter yor number')
+// if (num > 100) {
+//     alert('Your number > 100')
+// } else if(num < 100) {
+//     alert('Your number < 100')
+// } else {
+//     alert('Your number = 100');
+// }
+
+// -----------------------------------------------------------
+
+// const name = prompt('Enter your name!')
+// const age = +prompt('Enter your age!')
+// if (age <= 18){
+//     console.log();(`Hi,  ${name}`)
+// } else {
+//     console.log();(`Hello, ${name}`);
+// }
